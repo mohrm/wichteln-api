15a87da8a89855e93db3921a385a51d075eee82d
Martin Mohr <martin.mohr@posteo.de>
2023-08-20 17:49:51 +0200

    move preview page so that the apispec can be considered in isolation

--- preview/preview.js
+++ preview/preview.js
@@ -2,12 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('apispec'))
+app.use(express.static('index'))
+app.use('/apispec', express.static('../apispec'))
 app.get('/', (req, res) => {
-  res.redirect('index.html')
+  res.redirect('index/index.html')
 })
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
